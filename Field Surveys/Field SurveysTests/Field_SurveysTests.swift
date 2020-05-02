851860a692dc53c4b4f8b721ff96a4e4265e7ff3
Jonathan Van Egeren <jdv4nb@mail.missouri.eedu>
2020-05-01 21:51:27 -0500

    Start of Field Surveys project

---
+++ Field Surveys/Field SurveysTests/Field_SurveysTests.swift
@@ -0,0 +1,34 @@
+//
+//  Field_SurveysTests.swift
+//  Field SurveysTests
+//
+//  Created by Jonathan Van Egeren on 5/1/20.
+//  Copyright © 2020 Jonathan Van Egeren. All rights reserved.
+//
+
+import XCTest
+@testable import Field_Surveys
+
+class Field_SurveysTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
